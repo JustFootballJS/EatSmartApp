c5b8743bc9bb808b4e71874bc45cff5694b06327
SvetozarMateev <vidic9898@gmail.com>
2017-10-19 03:12:17 +0300

    more unit tests

---
+++ HealthyEating.Tests/Core/Commands/UserCommands/ChangeCurrentWeightCommandTests/Execute_Should.cs
@@ -0,0 +1,52 @@
+﻿using HealthyEating.Client.Core.Commands;
+using HealthyEating.Client.Core.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthyEating.Tests.Core.Commands.UserCommands.ChangeCurrentWeightCommandTests
+{
+    [TestClass]
+    public class Execute_Should
+    {
+        [TestMethod]
+        public void InvokeUserManagerCommand_WhenParamatersAreCorrect()
+        {
+            //Arrange
+            var readerMock = new Mock<IReader>();
+            var writerMock = new Mock<IWriter>();
+            var userManagerMock = new Mock<IUserManager>();
+
+            var listMock = new List<string>();
+            readerMock.Setup(x => x.Read()).Returns(It.IsAny<double>().ToString());
+            var command = new ChangeCurrentWeightCommand(readerMock.Object,writerMock.Object, userManagerMock.Object);
+
+            //Act
+            command.Execute(listMock);
+
+            //Assert
+            userManagerMock.Verify(x => x.ChangeCurrentWeight(It.IsAny<double>()), Times.Once);
+        }
+
+        [TestMethod]
+        public void ThrowFormatException_WhenParametersAreNotCorrect()
+        {
+            //Arrange
+            var readerMock = new Mock<IReader>();
+            var writerMock = new Mock<IWriter>();
+            var userManagerMock = new Mock<IUserManager>();
+
+            var listMock = new List<string>();
+            readerMock.Setup(x => x.Read()).Returns("Not a number");
+            var command = new ChangeCurrentWeightCommand(readerMock.Object, writerMock.Object, userManagerMock.Object);
+
+            //Act&Assert
+            Assert.ThrowsException<FormatException>(() => command.Execute(listMock));
+           
+        }
+    }
+}
---
+++ HealthyEating.Tests/Core/Commands/UserCommands/LogoutCommandTests/Execute_Should.cs
@@ -0,0 +1,37 @@
+﻿using HealthyEating.Client.Core.Commands.UserCommands;
+using HealthyEating.Client.Core.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthyEating.Tests.Core.Commands.UserCommands.LogoutCommandTests
+{
+    [TestClass]
+    public class Execute_Should
+    {
+        [TestMethod]
+        public void InvokeManagerCommand_WhenCalled()
+        {
+
+            //Arrange
+            var readerMock = new Mock<IReader>();
+            var writerMock = new Mock<IWriter>();
+            var userManagerMock = new Mock<IUserManager>();
+
+            var listMock = new List<string>();
+            
+            var command = new LogoutCommand(readerMock.Object, writerMock.Object, userManagerMock.Object);
+
+            //Act
+            command.Execute(listMock);
+
+            //Assert
+            userManagerMock.Verify(x => x.Logout(), Times.Once);
+
+        }
+    }
+}
